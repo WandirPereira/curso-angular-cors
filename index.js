72c05de5a8963cd75a194d680dddf4ca15421845
WandirPereira <wandir@gmail.com>
2024-02-16 01:27:47 -0300

    Configurando response

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
 server.use(express.json());
 
+server.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    console.log("OPTIONS");
+    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8080");
+    res.setHeader("Access-Control-Allow-Methods", "PUT");
+    res.setHeader("Access-Control-Allow-Headers", "content-type,x-auth-custom");
+  }
+  next();
+});
+
 const users = [
-    {
-        id: 0,
-        name: 'Felipe',
-        age: 25
-    },
+  {
+    id: 0,
+    name: "Felipe",
+    age: 25,
+  },
 ];
 
-server.put('/users/:id', (req, res) => {
-    const id = +req.params.id;
-    const newUser = req.body;
+server.put("/users/:id", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8080");
+  const id = +req.params.id;
+  const newUser = req.body;
 
-    const user = users.find(u => u.id === id);
+  const user = users.find((u) => u.id === id);
 
-    if(user) {
-        users[id] = newUser;
+  if (user) {
+    users[id] = newUser;
 
-        res.status(200).send(users[id]);
-    } else {
-        res.status(404).send({ message: 'Usuário não encontrado' });
-    }
+    res.status(200).send(users[id]);
+  } else {
+    res.status(404).send({ message: "Usuário não encontrado" });
+  }
 });
 
 server.listen(5000);
